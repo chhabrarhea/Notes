c56a1b7508f1ac280d20917c254280f009ee8a4a
Rhea Chhabra <rheachhabra1011@gmail.com>
2020-05-05 14:31:34 +0530

    Update add.java

--- app/src/main/java/com/example/notes/add.java
+++ app/src/main/java/com/example/notes/add.java
@@ -10,36 +10,60 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class add extends AppCompatActivity {
-SharedPreferences sp;
-Button b;
+ public class add extends AppCompatActivity {
+    SharedPreferences s;
+     Button b;
+    public static ArrayList<String> content;
+    int t;
+     
+     
+private void saveContent()
+{
+    try {
+        s.edit().putString("add",ObjectSerializer.serialize(content)).apply();
 
+        Log.i("add",ObjectSerializer.serialize(content));
 
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+}
+    
+
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        s=getSharedPreferences("package com.example.notes;",Context.MODE_PRIVATE);
+        content=MainActivity.editContent;
+        saveContent();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
 
         Intent in=getIntent();
-        int t=in.getIntExtra("noteId",-1);
-        sp=this.getSharedPreferences("com.example.notes", Context.MODE_PRIVATE);
-        final String l=((Object)t).toString();
+        t=in.getIntExtra("noteId",-1);
+
+        final String l=(content.get(t));
         final EditText et=(EditText) findViewById(R.id.editText);
-        et.setText(sp.getString((l),""));
+        et.setText(l);
+
         b=(Button) findViewById(R.id.button);
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                sp.edit().putString(l, String.valueOf(et.getText())).apply();
+                content.set(t,et.getText().toString());
                 back();
             }
         });
 
     }
+     
+     //return to main screen
     private void back()
     {
 
+        saveContent();
         Intent i=new Intent(add.this,MainActivity.class);
+        i.putExtra("changed",t);
         startActivity(i);
 
     }
